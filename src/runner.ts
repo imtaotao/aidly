6b1d3530435552c263ca6665d2d2752e7444f80e
chentao <dwchentao@163.com>
2025-08-09 21:18:24 +0800

    release: v1.36.0

--- src/runner.ts
+++ src/runner.ts
@@ -1,32 +1,49 @@
 import { retry } from './index';
+import { Result, type ResultType } from './result';
 import { isNil, isPromise, isResultType } from './is';
 
 const INTERNAL = Symbol('Runner');
 
+export type RunnerReturnType<B, R> = B extends true
+  ? ResultType<Awaited<R>>
+  : R;
+
 export interface RunnerOptions<
   T extends number | bigint = number,
   E = unknown,
+  B extends boolean = false,
 > {
+  result?: B;
   defaultDuration?: T;
   now?: () => T;
   diff?: (t1: T, t2: T) => T;
-  onBefore?: (r: Runner<T, E>, extra?: E) => void;
-  onAfter?: (r: Runner<T, E>, extra?: E, respOrErr?: unknown) => void;
+  onBefore?: (r: Runner<T, E, B>, extra?: E) => void;
+  onAfter?: (
+    r: Runner<T, E, B>,
+    extra: E | undefined,
+    respOrErr: RunnerReturnType<B, unknown>,
+  ) => void;
 }
 
-export class Runner<T extends number | bigint = number, E = unknown> {
+export class Runner<
+  T extends number | bigint = number,
+  E = unknown,
+  B extends boolean = false,
+> {
+  private _result: B;
   private _called = false;
-  private _onBefore?: RunnerOptions<T, E>['onBefore'];
-  private _onAfter?: RunnerOptions<T, E>['onAfter'];
-  private _now: NonNullable<RunnerOptions<T, E>['now']>;
-  private _diff: NonNullable<RunnerOptions<T, E>['diff']>;
+  private _onBefore?: RunnerOptions<T, E, B>['onBefore'];
+  private _onAfter?: RunnerOptions<T, E, B>['onAfter'];
+  private _now: NonNullable<RunnerOptions<T, E, B>['now']>;
+  private _diff: NonNullable<RunnerOptions<T, E, B>['diff']>;
 
   public duration: T;
   public code: '0' | '-1' = '0';
 
-  public constructor(options?: RunnerOptions<T, E>) {
+  public constructor(options?: RunnerOptions<T, E, B>) {
     this._onAfter = options?.onAfter;
     this._onBefore = options?.onBefore;
+    this._result = options?.result || (false as B);
     this._now = options?.now || (Date.now as () => T);
     this._diff =
       options?.diff || (((t1, t2) => t2 - t1) as (t1: T, t2: T) => T);
@@ -39,6 +56,26 @@ export class Runner<T extends number | bigint = number, E = unknown> {
     if (flag === INTERNAL) {
       this.duration = this._diff(start, this._now());
     }
+  }
+
+  private _tryWrapOkResult<R>(val: R, isResult: boolean) {
+    if (isResult) return val;
+    return this._result ? Result.create().ok(val) : val;
+  }
+
+  private _tryWrapErrorResult(err: any) {
+    if (this._result && !isResultType(err)) {
+      return {
+        tryThrow: () => {},
+        err: Result.create().error(err),
+      };
+    }
+    return {
+      err,
+      tryThrow: () => {
+        throw err;
+      },
+    };
   }
 
   /**
@@ -54,7 +91,7 @@ export class Runner<T extends number | bigint = number, E = unknown> {
    * 3. Since we cannot control the implementation of external thenable,
    *    safely wrapping or repeatedly calling `.then` is not feasible.
    */
-  public run<R>(fn: () => R, extra?: E, flag?: symbol) {
+  public run<R>(fn: () => R, extra?: E, flag?: symbol): RunnerReturnType<B, R> {
     if (this._called) {
       throw new Error('Runner can only be called once');
     }
@@ -63,11 +100,14 @@ export class Runner<T extends number | bigint = number, E = unknown> {
     this._onBefore?.(this, extra);
 
     try {
-      let res = fn();
+      let res: any = fn();
+
       if (isPromise(res)) {
-        res = res.then(
-          (val) => {
-            this.code = isResultType(val) ? (val.ok ? '0' : '-1') : '0';
+        return res.then(
+          (val: any) => {
+            const isResult = isResultType(val);
+            this.code = isResult ? (val.ok ? '0' : '-1') : '0';
+            val = this._tryWrapOkResult(val, isResult);
             this._setDuration(start, flag);
             this._onAfter?.(this, extra, val);
             return val;
@@ -75,21 +115,27 @@ export class Runner<T extends number | bigint = number, E = unknown> {
           (e) => {
             this.code = '-1';
             this._setDuration(start, flag);
-            this._onAfter?.(this, extra, e);
-            throw e;
+            const obj = this._tryWrapErrorResult(e);
+            this._onAfter?.(this, extra, obj.err);
+            obj.tryThrow();
+            return obj.err;
           },
-        ) as R;
+        ) as RunnerReturnType<B, R>;
       } else {
-        this.code = isResultType(res) ? (res.ok ? '0' : '-1') : '0';
+        const isResult = isResultType(res);
+        this.code = isResult ? (res.ok ? '0' : '-1') : '0';
         this._setDuration(start, flag);
+        res = this._tryWrapOkResult(res, isResult);
         this._onAfter?.(this, extra, res);
+        return res;
       }
-      return res;
-    } catch (e) {
+    } catch (e: any) {
       this.code = '-1';
       this._setDuration(start, flag);
-      this._onAfter?.(this, extra, e);
-      throw e;
+      const obj = this._tryWrapErrorResult(e);
+      this._onAfter?.(this, extra, obj.err);
+      obj.tryThrow();
+      return obj.err;
     }
   }
 
@@ -114,7 +160,7 @@ export class Runner<T extends number | bigint = number, E = unknown> {
   }
 
   public clone() {
-    return Runner.create<T, E>({
+    return Runner.create<T, E, B>({
       now: this._now,
       diff: this._diff,
       onAfter: this._onAfter,
@@ -122,9 +168,11 @@ export class Runner<T extends number | bigint = number, E = unknown> {
     });
   }
 
-  public static create<CT extends number | bigint = number, CE = unknown>(
-    options?: RunnerOptions<CT, CE>,
-  ) {
-    return new Runner<CT, CE>(options);
+  public static create<
+    CT extends number | bigint = number,
+    CE = unknown,
+    CB extends boolean = false,
+  >(options?: RunnerOptions<CT, CE, CB>) {
+    return new Runner<CT, CE, CB>(options);
   }
 }
